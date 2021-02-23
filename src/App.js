53cea24c0828509cab4e7ad985fc5135d1b00e2f
randriani <randriani@brasilseg.com.br>
2021-02-23 15:26:41 -0300

    Pre Redux

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ class App extends Component{
               id: snapshot.id,
               ...snapshot.data()
           }})
-          console.log(this.state);
         })
       }
       this.setState({ currentUser: userAuth });
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils.js';
+import {auth, signInWithGoogle } from '../../firebase/firebase.utils.js';
 import './sign-in.styles.scss';
 
 class SignIn extends Component{
@@ -14,10 +14,17 @@ class SignIn extends Component{
         }
     }
 
-    handleSubmit = (e) => {
+    handleSubmit = async (e) => {
         e.preventDefault();
 
-        this.setState({email : '', password: ''});
+        const { email, password } = this.state;
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email : '', password: ''});
+        }
+        catch(e){
+            console.error(e);
+        }
     }
 
     handleChange = (e) => {
