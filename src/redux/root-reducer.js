78b22c6d209a396f29f05973d774b218033747a2
RodrigoBertiniAndriani <S3rv1c3G1thub>
2021-03-01 16:02:55 -0300

    Session Storage com redux-storage - ok

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,21 @@
 import { combineReducers } from 'redux';
+
+// Persistance
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'] // Libera o que deve ser persistido
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
